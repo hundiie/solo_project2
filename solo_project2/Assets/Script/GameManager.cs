8dcd9b5168493eeeb4946e4d727bdb153b0df0c8
M0000000N <ldh1923@naver.com>
2022-09-20 10:10:13 +0900

    자리옮김

--- solo_project2/Assets/Script/GameManager.cs
+++ solo_project2/Assets/Script/GameManager.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private void Start()
+    {
+        
+    }
     private void Update()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         if (Input.GetKeyDown(KeyCode.P))
         {
             SceneManager.LoadScene(0);
--- solo_project2/Assets/Script/Player/CameraMove.cs
+++ solo_project2/Assets/Script/Player/CameraMove.cs
@@ -38,7 +38,7 @@ public class CameraMove : MonoBehaviour
         Debug.DrawRay(transform.position, transform.forward * 100f, Color.red);
         
         Ray ray = Camera.main.ScreenPointToRay(_CameraCenter);
-        RaycastHit[] hit = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.forward,1);
+        RaycastHit[] hit = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.forward);
 
         for (int i = 0; i < hit.Length; i++)
         {
@@ -46,9 +46,9 @@ public class CameraMove : MonoBehaviour
 
             if (hitChecker.collider.gameObject.transform.tag == "Tile")
             {
-                if (Player.GetComponent<PlayerCreate>().HitObject != hitChecker.collider.gameObject.transform.gameObject)
+                if (Player.GetComponent<PlayerCreate>().HitObject != hitChecker.collider.gameObject)
                 {
-                    Player.GetComponent<PlayerCreate>().HitObject = hitChecker.collider.gameObject.transform.gameObject;
+                    Player.GetComponent<PlayerCreate>().HitObject = hitChecker.collider.gameObject;
                 }
                 return;
             }
